38946ed3c776e0950be62e1762fa9b3f7cc1c527
Paulo Salvatore <salvatoregames@gmail.com>
2022-02-10 09:35:30 -0300

    Initial commit

---
+++ app/src/main/java/com/oceanbrasil/jornada_android_fev22_pushnotification/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.oceanbrasil.jornada_android_fev22_pushnotification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
